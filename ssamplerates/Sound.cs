03e8c0eb0989a2a3023b9d7dbd56c2f846ceec38
JustinRoll <justin.j.roll@gmail.com>
2012-07-22 13:53:39 -0700

    Base commit of VS 2010 project with so Sound class

---
+++ ssamplerates/Sound.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Sound
+{
+    public float intensity { get; set; }
+    public float frequency { get; set; }
+    public float length { get; set; }
+    public String fileName { get; set; }
+    
+
+	public Sound()
+	{
+	}
+}
